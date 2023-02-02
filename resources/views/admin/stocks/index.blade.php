85716b931899102ae3f20d3ced7d9d7cf948890e
himanshu-c247 <himanshu.d@chapter247.com>
2023-02-02 16:52:10 +0530

    feat:changes in stock

--- resources/views/admin/stocks/index.blade.php
+++ resources/views/admin/stocks/index.blade.php
@@ -2,15 +2,27 @@
 @section('content')
 @include('sweetalert::alert')
 <div class="card">
-    <div class="card-header d-flex justify-content-between">
+    <div class="card-header d-flex justify-content-between align-items-center">
         <h4>{{ trans('cruds.stock.title_singular') }} {{ trans('global.list') }}</h4>
         @can('permission_create')
-        <div class="d-flex justify-content-between">
-         <button class="btn btn-primary stock-model" data-url="{{ route("admin.stocks.create") }}">{{ trans('global.add') }} {{ trans('cruds.stock.title_singular') }}</button>
+        <div class="filter-search-block d-flex justify-content-between">
+            <form method="GET" id="search-form" action="{{route('admin.stocks.index')}}" autocomplete="off">
+                <div class="row">
+                    <div class="form-group search-group">
+                        <div class="search-box">
+                            <input type="text" id="search" name="search" value="{{ app('request')->input('search') }}" class="form-control" placeholder="Search by product">
+                            <i class="ri-search-line search-icon"></i>
+                            <div class="search-via">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <a href=""><button type="button" class="reset-btn btn btn-primary ml-3" data-toggle="tooltip" data-placement="top" title="Reset"><i class="fa fa-refresh text-white"></i></button></a> 
+             <a><button class="btn btn-primary stock-model ml-2" data-url="{{ route("admin.stocks.create") }}">{{ trans('global.add') }} {{ trans('cruds.stock.title_singular') }}</button></a>
         </div>
         @endcan
     </div>
-
     <div class="card-body">
         <div class="table-responsive">
             <table class=" table table-bordered table-striped table-hover">
@@ -38,17 +50,16 @@
                         <th>
                             Unit
                         </th>
+                        {{-- <th class="text-center">
+                            Add Stock
+                        </th> --}}
                         @user
-                            <th class="text-center">
-                                Add Stock
-                            </th>
                             <th class="text-center">
                                 Remove Stock
                             </th>
                         @enduser
                         <th class="text-center">
-                            {{ trans('cruds.stock.fields.action') }}
-                            
+                            {{ trans('cruds.stock.fields.action') }}   
                         </th>
                     </tr>
                 </thead>
@@ -64,12 +75,30 @@
 @parent
 <script src="{{ asset('js/stock.js') }}"></script>
 <script>
-$('.remove-stock').click(function() {
-            var url = $(this).attr('data-url');
-            var value = $(this).parents("tr").find('.stock-value').val();
-            var currentStock = $(this).attr('data-current-stock');
-            if(currentStock - value < 0){
-                Swal.fire({
+    /* =========== Leave History Search =========== */
+    var searchFilter = function() {
+        var form_action = $("#search-form").attr("action");
+        $.ajax({
+            url: form_action,
+            type: "GET",
+            dataType: 'json',
+            data: $('#search-form').serialize(),
+            success: function(data) {
+                $('.stock-table').html(data.stockSearch);
+            },
+        });
+    }
+    $(document).on('keyup', '#search', function() {
+        searchFilter();
+    });
+</script>
+<script>
+    $('.remove-stock').click(function() {
+        var url = $(this).attr('data-url');
+        var value = $(this).parents("tr").find('.stock-value').val();
+        var currentStock = $(this).attr('data-current-stock');
+        if (currentStock - value < 0) {
+            Swal.fire({
                 title: 'Opss...',
                 text: "Not enough items in stock!",
                 icon: 'warning',
@@ -77,10 +106,9 @@ $('.remove-stock').click(function() {
                 confirmButtonColor: '#3085d6',
                 cancelButtonColor: '#d33',
                 confirmButtonText: 'Inform Admin!'
-                })
-            }
-            else{
-                $.ajax({
+            })
+        } else {
+            $.ajax({
                 url: url,
                 method: 'POST',
                 data: $('#removeStockForm').serialize(),
@@ -92,31 +120,33 @@ $('.remove-stock').click(function() {
                             timeOut: '4000',
                         }),
                         location.reload();
-                    } 
-            }) 
-            }
-          
+                }
+            })
+        }
+
     });
 </script>
 <script>
-    $(function () {
-  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
+    $(function() {
+        let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
 
-  $.extend(true, $.fn.dataTable.defaults, {
-    pageLength: 100,
-      columnDefs: [{
-          orderable: true,
-          className: '',
-          targets: 0
-      }]
-  });
-  $('.datatable-Stock:not(.ajaxTable)').DataTable({ buttons: dtButtons })
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
-        $($.fn.dataTable.tables(true)).DataTable()
-            .columns.adjust();
-    });
-})
-
+        $.extend(true, $.fn.dataTable.defaults, {
+            pageLength: 100,
+            columnDefs: [{
+                orderable: true,
+                className: '',
+                targets: 0
+            }]
+        });
+        $('.datatable-Stock:not(.ajaxTable)').DataTable({
+            buttons: dtButtons
+        })
+        $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+            $($.fn.dataTable.tables(true)).DataTable()
+                .columns.adjust();
+        });
+    })
 </script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 @endsection
---
+++ resources/views/admin/stocks/stock-history-table.blade.php
@@ -0,0 +1,11 @@
+@foreach($stock->asset->transactions as $transaction)
+<tr>
+    <td class=text-center> {{ $loop->index + 1 }}</td>
+    <td>{{ $stock->asset->name ?? 'NA' }}</td>
+    <td class="text-capitalize">
+        {{ $transaction->team->name }}
+    </td>
+    <td class="text-center">{{ $transaction->stock }}</td>
+    <td class="text-center">{{ dateFormat($transaction->created_at) ?? 'NA'}}</td>
+</tr>
+@endforeach
